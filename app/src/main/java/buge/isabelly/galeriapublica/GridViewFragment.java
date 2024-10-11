d241db85f7f3128a7229faca89b770415b1035a4
Isabelly Eduarda Buge <isabellyeduardabuge@gmail.com>
2024-10-11 10:57:19 -0300

    aula 11/10 - Passo 7

--- app/src/main/java/buge/isabelly/galeriapublica/GridViewFragment.java
+++ app/src/main/java/buge/isabelly/galeriapublica/GridViewFragment.java
@@ -28,21 +28,11 @@ public class GridViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GridViewFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static GridViewFragment newInstance(String param1, String param2) {
+    public static GridViewFragment newInstance() {
         GridViewFragment fragment = new GridViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
--- app/src/main/java/buge/isabelly/galeriapublica/MainActivity.java
+++ app/src/main/java/buge/isabelly/galeriapublica/MainActivity.java
@@ -1,14 +1,26 @@
 package buge.isabelly.galeriapublica;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;// define o bottomViewNavigation como um atributo da classe MainActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);//referenciando MainViewModel
+
+        bottomNavigationView = findViewById(R.id.btNav);//referenciando bottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {//seta bottonNavigationView como “escutador” de eventos
+
+            //quando selecionada uma das opcoes o metodo eh executado
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {//indica opcao escolhida
+                vm.setNavigationOpSelected(item.getItemId());//guarda no MainViewModel a opcao escolhida
+
+                //define acoes a ser realizadas para cada opcao
+                switch (item.getItemId()) {
+                    case R.id.gridViewOp:
+                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                        setFragment(gridViewFragment);
+                        break;
+                    case R.id.listViewOp:
+                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                        setFragment(listViewFragment);
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
     }
 }
